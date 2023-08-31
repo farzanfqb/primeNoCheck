90fc3f6a727cdef2056aa6e8115bde74efeac6a2
farzanfqb <farzanfqb@gmail.com>
2023-08-31 14:52:51 +0530

    ffinal

--- Main.java
+++ Main.java
@@ -1,58 +1,74 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-/*class Rev {    public void method() {
-        for (int i = 0; i <= Array.array.length - 1; i++) {
-            System.out.println(Array.array[Array.array.length - 1 - i]);
-        }
-    }
-}*/
-class AddCheck{
-    public void method(int[] array, int target){
-        for (int i=0; i<array.length; i++) {
-            for (int j=1 ; j<array.length; j++) {
-                if (j>i && array[i]+array[j] == Target.target ){
-                    System.out.println( array[i] + " " + array[j] );
-            } else  continue ;
-        }
+class Rev {
+    public void method(List<Integer> array) {
+        for (int i = array.size() - 1; i >= 0; i--) {
+            System.out.println(array.get(i));
         }
     }
 }
-class Target{
+
+class AddCheck {
+    public void method(List <Integer> array, int target) {
+        for (int i = 0; i < array.size()-1; i++) {
+            for (int j = i+1; j < array.size(); j++) {
+                if (array.get(i) +array.get(j) == target) {
+                    System.out.println(array.get(i) + " " + array.get(j));
+                } 
+            }
+        }
+    }
+}
+class Target {
     static int target;
+
     public static void getTarget(Scanner scanner) {
-    do{       
-         System.out.println("Enter Target");
-        try {
-            target = scanner.nextInt();
-            break;
+        do {
+            System.out.println("Enter Target");
+            try {
+                target = scanner.nextInt();
+                break;
+            } catch (Exception e) {
+                System.out.println(e);
+                System.out.println("Enter Valid Integer");
+                scanner.nextLine();
+            }
+        } while (true);
     }
-        catch (Exception e) {
-            System.out.println(e);
-        System.out.println("Enter Valid Integer");
-        scanner.nextLine();
-    }} while(true);
 }
-}
-class Array{
-    public static int[] array = new int [4];
-    public  static void getArray(Scanner scanner){
+
+class Array {
+    public static List<Integer> array = new ArrayList<>();
+
+    public static void getArray(Scanner scanner) {
         System.out.println("Enter elements in an ARRAY");
-        for (int i=0; i<array.length; i++){
-        try { 
-         array[i] = scanner.nextInt();
-        }    catch (Exception e) {
-            System.out.println("Enter valid Integers");
-            i--;
+        while(true) {
+            String next = scanner.next();
+            if (next.equals("end"))
+                break;
+            else {
+                try {
+                    int num = Integer.parseInt(next);
+                    array.add(num);
+                } catch (Exception e) {
+                    System.out.println("Enter valid Integers");
+                }
+            }
         }
-}}}
+    }
+}
+
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Array.getArray(scanner);
         AddCheck obj2 = new AddCheck();
         Target.getTarget(scanner);
-        obj2.method(Array.array, Target.target); 
-        
+        obj2.method(Array.array, Target.target);
+        Rev rev = new Rev();
+        rev.method(Array.array);
         scanner.close();
     }
 }
